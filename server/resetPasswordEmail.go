506d834f4f7b87d8cd3a0e58a7ac3a39ce1a5a66
Jason Lee Jian Ying <79128355+Jasonljy90@users.noreply.github.com>
2022-07-24 10:36:30 +0800

    Update template

--- server/resetPasswordEmail.go
+++ server/resetPasswordEmail.go
@@ -13,10 +13,11 @@ func sendEmail(email string, firstName string, lastName, link string) {
 	from := mail.NewEmail("Live Chat", os.Getenv("SENDER_EMAIL"))
 	subject := "Password Reset Request"
 	to := mail.NewEmail(firstName+" "+lastName, email)
-	plainTextContent := "You have received this email because a password reset request for Chat App account was received. The reset link will only be valid for 30mins. Click the link to reset your password: \r\n" + link
-	//htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
-	//message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
-	message := mail.NewSingleEmailPlainText(from, subject, to, plainTextContent)
+	//plainTextContent := "You have received this email because a password reset request for Chat App account was received. The reset link will only be valid for 30mins. Click on the button to reset your password: \r\n" + link
+	//message := mail.NewSingleEmailPlainText(from, subject, to, plainTextContent)
+	plainTextContent := "You have received this email because a password reset request for Chat App account was received. The reset link will only be valid for 30mins. Click on the button to reset your password: \r\n"
+	htmlContent := "<a href= " + link + ">Reset Password</a>"
+	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
 	client := sendgrid.NewSendClient(os.Getenv("SENDGRID_API_KEY"))
 	response, err := client.Send(message)
 	if err != nil {
